f8928c08acfdc9677ec22a011243066a4ac1e1fd
bjtlr0 <bjtlr@naver.com>
2019-05-30 17:44:32 +0900

    junit use autowired

--- src/test/java/com/module/test/hibernateTest.java
+++ src/test/java/com/module/test/hibernateTest.java
@@ -2,9 +2,11 @@ package com.module.test;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -14,10 +16,18 @@ import com.module.database.model.Account;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations={"file:src/main/resources/hibernate.cfg.xml"}
+@ContextConfiguration( // Autowired가 왜 안되는지 모르고 삽질하다가, 필요한 class를 선언해주면 된다는걸 알게됐다...
+		classes={HibernateSessionManager.class,
+				AccountDaoImpl.class
+				}
+		/*locations={"file:src/main/resources/hibernate.cfg.xml"}*/
 		)
 public class hibernateTest {
+	@Autowired
+	HibernateSessionManager hsManager;
+	
+	@Autowired
+	AccountDaoImpl accountDao;
 	
 	@Test @Ignore
 	public void test(){
@@ -28,10 +38,15 @@ public class hibernateTest {
 		
 		System.out.println(account.getAccountName());*/
 	}
+	
+	@Before
+	public void setUp(){
+		hsManager.buildSessionFactory();
+	}
+	
 	@Test
 	public void test2(){
-		HibernateSessionManager.buildSessionFactory();
-		AccountDaoImpl accountDao = new AccountDaoImpl();
+//		AccountDaoImpl accountDao = new AccountDaoImpl();
 		List<Account> list = accountDao.getAccountList();
 		System.out.println(list.size());
 	}
